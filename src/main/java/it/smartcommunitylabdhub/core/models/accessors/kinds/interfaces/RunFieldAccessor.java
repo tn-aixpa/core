1e3aff6e2083362d61996e1571b377974e83b808
Luca Trubbiani <luca.trubbiani@gmail.com>
2023-11-30 14:36:44 +0100

    Add Run / RunEntity Accessor fields

---
+++ src/main/java/it/smartcommunitylabdhub/core/models/accessors/kinds/interfaces/RunFieldAccessor.java
@@ -0,0 +1,5 @@
+package it.smartcommunitylabdhub.core.models.accessors.kinds.interfaces;
+
+public interface RunFieldAccessor<O extends RunFieldAccessor<O>> extends CommonFieldAccessor<O> {
+
+}
---
+++ src/main/java/it/smartcommunitylabdhub/core/models/accessors/kinds/runs/RunDefaultFieldAccessor.java
@@ -0,0 +1,13 @@
+package it.smartcommunitylabdhub.core.models.accessors.kinds.runs;
+
+import it.smartcommunitylabdhub.core.annotations.common.AccessorType;
+import it.smartcommunitylabdhub.core.components.infrastructure.enums.EntityName;
+import it.smartcommunitylabdhub.core.models.accessors.kinds.abstracts.AbstractFieldAccessor;
+import it.smartcommunitylabdhub.core.models.accessors.kinds.interfaces.RunFieldAccessor;
+
+
+@AccessorType(kind = "run", entity = EntityName.RUN)
+public class RunDefaultFieldAccessor
+        extends AbstractFieldAccessor<RunDefaultFieldAccessor>
+        implements RunFieldAccessor<RunDefaultFieldAccessor> {
+}
