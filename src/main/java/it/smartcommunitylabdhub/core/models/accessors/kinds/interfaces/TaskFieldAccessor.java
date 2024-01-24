da0e2cf8ed3fae26471f020e2c27b2bccc43672c
Luca Trubbiani <luca.trubbiani@gmail.com>
2024-01-24 11:37:17 +0100

    Working on container with job and deploy task. Fix field on TaskBaseSpec

--- src/main/java/it/smartcommunitylabdhub/core/models/accessors/kinds/interfaces/TaskFieldAccessor.java
+++ src/main/java/it/smartcommunitylabdhub/core/models/accessors/kinds/interfaces/TaskFieldAccessor.java
@@ -11,7 +11,7 @@ public interface TaskFieldAccessor<O extends TaskFieldAccessor<O>> extends Commo
     }
 
     @SuppressWarnings("unchecked")
-    default Map<String, Object> getVolumeMounts() {
+    default Map<String, Object> getNodeSelector() {
         return mapHasField(getSpecs(), "node_selector")
                 ? (Map<String, Object>) getSpecs().get("node_selector")
                 : null;
