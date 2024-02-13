0c5f1f967b80b41c08e155d0979276ebd824f8e3
Luca Trubbiani <luca.trubbiani@gmail.com>
2024-02-13 10:03:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Change directory to the parent directory of modules
+
 cd modules || exit 1
 
-# Build each module, skipping tests
+# Build each modules
 for module in commons fsm framework-k8s runtime-container runtime-dbt runtime-mlrun runtime-nefertem; do
     echo "Building module: $module"
     cd "$module" || exit 1
@@ -13,12 +13,12 @@ done
 
 cd .. || exit 1
 
-# Go back to the application folder and build it
+# Build Application Module
 cd application || exit 1
 echo "Building application module"
 mvn clean install -DskipTests
 
-# Go back to the root folder and build it
+# Build Root Project
 cd .. || exit 1
 echo "Building root project"
 mvn clean install -DskipTests
@@ -26,4 +26,4 @@ mvn clean install -DskipTests
 # Run the application
 cd application || exit 1
 echo "Running Spring Boot application"
-mvn spring-boot:run
+mvn spring-boot:run  # -Dspring-boot.run.profiles=local (to pass profile)
