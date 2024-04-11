7c1901b605a99e2cc3f01e8e6cd2fec3790a1910
micnori <micnori@users.noreply.github.com>
2024-04-11 11:49:19 +0200

    add solr context search

---
+++ application/src/main/java/it/smartcommunitylabdhub/core/controllers/v1/context/SolrContextController.java
@@ -0,0 +1,87 @@
+package it.smartcommunitylabdhub.core.controllers.v1.context;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import it.smartcommunitylabdhub.commons.Keys;
+import it.smartcommunitylabdhub.core.annotations.ApiVersion;
+import it.smartcommunitylabdhub.core.components.solr.ItemResult;
+import it.smartcommunitylabdhub.core.components.solr.SearchGroupResult;
+import it.smartcommunitylabdhub.core.components.solr.SolrComponent;
+import it.smartcommunitylabdhub.core.components.solr.SolrPage;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@ApiVersion("v1")
+@RequestMapping("/-/{project}/solr")
+@PreAuthorize(
+    "hasAuthority('ROLE_ADMIN') or (hasAuthority(#project+':ROLE_USER') or hasAuthority(#project+':ROLE_ADMIN'))"
+)
+@Validated
+@Slf4j
+@Tag(name = "Solr search context API", description = "Endpoints related to Solr search")
+public class SolrContextController {
+	
+    @Autowired(required = false)
+    SolrComponent solrComponent;
+    
+    @GetMapping(path = "/search/group", produces = "application/json; charset=UTF-8")
+    public ResponseEntity<SolrPage<SearchGroupResult>> searchGroup(
+    		@PathVariable @Valid @NotNull @Pattern(regexp = Keys.SLUG_PATTERN) String project,
+    		@RequestParam(required = false) String q,
+    		@RequestParam(required = false) List<String> fq,
+    		Pageable pageRequest
+    ) {
+        if (solrComponent == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        }
+        try {
+            SolrPage<SearchGroupResult> page = solrComponent.groupSearch(q, setProject(fq, project), pageRequest);
+            return ResponseEntity.ok(page);
+        } catch (Exception e) {
+            log.error(String.format("searchGroup:", e.getMessage()));
+            return ResponseEntity.ok(null);
+        }
+    }
+
+    @GetMapping(path = "/search/item", produces = "application/json; charset=UTF-8")
+    public ResponseEntity<SolrPage<ItemResult>> search(
+    		@PathVariable @Valid @NotNull @Pattern(regexp = Keys.SLUG_PATTERN) String project,
+    		@RequestParam(required = false) String q,
+    		@RequestParam(required = false) List<String> fq,
+    		Pageable pageRequest
+    ) {
+        if (solrComponent == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        }
+
+        try {
+            SolrPage<ItemResult> page = solrComponent.itemSearch(q, setProject(fq, project), pageRequest);
+            return ResponseEntity.ok(page);
+        } catch (Exception e) {
+            log.error(String.format("search:", e.getMessage()));
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    private List<String> setProject(List<String> fq, String project) {
+    	fq.replaceAll(element -> element.startsWith("project:") ? "project:" + project : element);
+    	return fq;
+    }
+    
+}
