073ce75726564f2e3b2dd46340e45240875e730b
Matteo Saloni <matteo.saloni@gmail.com>
2024-06-20 14:10:23 +0200

    fix: align python builders logic

--- modules/runtime-python/src/main/java/it/smartcommunitylabdhub/runtime/python/builders/PythonJobBuilder.java
+++ modules/runtime-python/src/main/java/it/smartcommunitylabdhub/runtime/python/builders/PythonJobBuilder.java
@@ -4,18 +4,14 @@ import it.smartcommunitylabdhub.commons.infrastructure.Builder;
 import it.smartcommunitylabdhub.runtime.python.specs.function.PythonFunctionSpec;
 import it.smartcommunitylabdhub.runtime.python.specs.run.PythonRunSpec;
 import it.smartcommunitylabdhub.runtime.python.specs.task.PythonJobTaskSpec;
-
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class PythonJobBuilder implements Builder<PythonFunctionSpec, PythonJobTaskSpec, PythonRunSpec> {
 
     @Override
     public PythonRunSpec build(PythonFunctionSpec funSpec, PythonJobTaskSpec taskSpec, PythonRunSpec runSpec) {
-
-
         Map<String, Serializable> map = new HashMap<>();
         map.putAll(runSpec.toMap());
         map.putAll(funSpec.toMap());
--- modules/runtime-python/src/main/java/it/smartcommunitylabdhub/runtime/python/builders/PythonServeBuilder.java
+++ modules/runtime-python/src/main/java/it/smartcommunitylabdhub/runtime/python/builders/PythonServeBuilder.java
@@ -4,21 +4,21 @@ import it.smartcommunitylabdhub.commons.infrastructure.Builder;
 import it.smartcommunitylabdhub.runtime.python.specs.function.PythonFunctionSpec;
 import it.smartcommunitylabdhub.runtime.python.specs.run.PythonRunSpec;
 import it.smartcommunitylabdhub.runtime.python.specs.task.PythonServeTaskSpec;
-import java.util.Optional;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PythonServeBuilder implements Builder<PythonFunctionSpec, PythonServeTaskSpec, PythonRunSpec> {
 
     @Override
     public PythonRunSpec build(PythonFunctionSpec funSpec, PythonServeTaskSpec taskSpec, PythonRunSpec runSpec) {
-        PythonRunSpec pythonSpec = new PythonRunSpec(runSpec.toMap());
-        pythonSpec.setTaskServeSpec(taskSpec);
+        Map<String, Serializable> map = new HashMap<>();
+        map.putAll(runSpec.toMap());
+        map.putAll(funSpec.toMap());
+        taskSpec.toMap().forEach(map::putIfAbsent);
+
+        PythonRunSpec pythonSpec = new PythonRunSpec(map);
         pythonSpec.setFunctionSpec(funSpec);
-
-        //let run override k8s specs
-        Optional
-            .ofNullable(runSpec.getTaskServeSpec())
-            .ifPresent(k8sSpec -> pythonSpec.getTaskServeSpec().configure(k8sSpec.toMap()));
-
         return pythonSpec;
     }
 }
