0324ea5cdc05b4d6569e66c98b9ecfaa5688857e
Matteo Saloni <matteo.saloni@gmail.com>
2024-07-04 11:45:17 +0200

    feat: cleanup k8s listeners and fix errors

--- modules/framework-k8s/src/main/java/it/smartcommunitylabdhub/framework/k8s/listeners/K8sRunnableListener.java
+++ modules/framework-k8s/src/main/java/it/smartcommunitylabdhub/framework/k8s/listeners/K8sRunnableListener.java
@@ -2,41 +2,58 @@ package it.smartcommunitylabdhub.framework.k8s.listeners;
 
 import it.smartcommunitylabdhub.commons.events.RunnableChangedEvent;
 import it.smartcommunitylabdhub.commons.events.RunnableMonitorObject;
+import it.smartcommunitylabdhub.commons.exceptions.FrameworkException;
 import it.smartcommunitylabdhub.commons.exceptions.StoreException;
+import it.smartcommunitylabdhub.commons.infrastructure.RunRunnable;
 import it.smartcommunitylabdhub.commons.models.enums.State;
 import it.smartcommunitylabdhub.commons.services.RunnableStore;
-import it.smartcommunitylabdhub.framework.k8s.annotations.ConditionalOnKubernetes;
-import it.smartcommunitylabdhub.framework.k8s.exceptions.K8sFrameworkException;
-import it.smartcommunitylabdhub.framework.k8s.infrastructure.k8s.K8sDeploymentFramework;
-import it.smartcommunitylabdhub.framework.k8s.runnables.K8sDeploymentRunnable;
+import it.smartcommunitylabdhub.framework.k8s.infrastructure.k8s.K8sBaseFramework;
+import it.smartcommunitylabdhub.framework.k8s.runnables.K8sRunnable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-@Component
-@ConditionalOnKubernetes
 @Slf4j
-public class K8sDeploymentRunnableListener {
+public class K8sRunnableListener<R extends K8sRunnable> {
 
-    @Autowired
-    K8sDeploymentFramework k8sFramework;
+    private final Class<R> clazz;
 
-    @Autowired
-    ApplicationEventPublisher eventPublisher;
+    private final K8sBaseFramework<R, ?> k8sFramework;
 
-    @Autowired
-    private RunnableStore<K8sDeploymentRunnable> runnableStore;
+    private final RunnableStore<R> runnableStore;
+
+    private ApplicationEventPublisher eventPublisher;
+
+    @SuppressWarnings("unchecked")
+    public K8sRunnableListener(K8sBaseFramework<R, ?> k8sFramework, RunnableStore<R> runnableStore) {
+        Assert.notNull(k8sFramework, "k8sFramework can not be null");
+        Assert.notNull(runnableStore, "runnableStore can not be null");
+
+        this.k8sFramework = k8sFramework;
+        this.runnableStore = runnableStore;
+
+        this.clazz = (Class<R>) runnableStore.getResolvableType().resolve();
+        log.debug("started listener for {}", clazz.getName());
+    }
 
     @Async
     @EventListener
-    public void listen(K8sDeploymentRunnable runnable) {
+    public void listen(R runnable) {
         Assert.notNull(runnable, "runnable can not be null");
         Assert.hasText(runnable.getId(), "runnable id can not be null or empty");
-        log.info("Receive runnable for execution: {}", runnable.getId());
+        log.info(
+            "Receive runnable {} for execution: {} state {}",
+            clazz.getSimpleName(),
+            runnable.getId(),
+            runnable.getState()
+        );
+
+        if (log.isTraceEnabled()) {
+            log.trace("runnable {}: {}", clazz.getSimpleName(), runnable);
+        }
 
         try {
             runnable =
@@ -56,34 +73,42 @@ public class K8sDeploymentRunnableListener {
                 };
 
             if (runnable != null) {
+                if (log.isTraceEnabled()) {
+                    log.trace("runnable result from framework {}: {}", clazz.getSimpleName(), runnable);
+                }
+
                 try {
                     // If runnable is deleted, remove from store
                     if (runnable.getState().equals(State.DELETED.name())) {
+                        log.debug("remove runnable {} {} from store", clazz.getSimpleName(), runnable.getId());
                         runnableStore.remove(runnable.getId());
                     } else {
+                        log.debug("update runnable {} {} in store", clazz.getSimpleName(), runnable.getId());
                         runnableStore.store(runnable.getId(), runnable);
                     }
                 } catch (StoreException e) {
                     log.error("Error with store: {}", e.getMessage());
                 }
             }
-        } catch (K8sFrameworkException e) {
+        } catch (FrameworkException e) {
             // Set runnable to error state send event
-            log.error("Error with k8s: {}", e.getMessage());
+            log.error("Error with k8s for runnable {} {}: {}", clazz.getSimpleName(), runnable.getId(), e.getMessage());
             runnable.setState(State.ERROR.name());
 
             try {
+                log.debug("update runnable {} {} in store", clazz.getSimpleName(), runnable.getId());
                 runnableStore.store(runnable.getId(), runnable);
             } catch (StoreException se) {
                 log.error("Error with store: {}", se.getMessage());
             }
         } finally {
             if (runnable != null) {
-                log.debug("Processed runnable {}", runnable.getId());
+                log.debug("Processed runnable {} {}", clazz.getSimpleName(), runnable.getId());
 
-                // Publish event to Run Manager
-                eventPublisher.publishEvent(
-                    RunnableChangedEvent
+                if (eventPublisher != null) {
+                    log.debug("Publish runnable {} {}", clazz.getSimpleName(), runnable.getId());
+
+                    RunnableChangedEvent<RunRunnable> event = RunnableChangedEvent
                         .builder()
                         .runnable(runnable)
                         .runMonitorObject(
@@ -96,9 +121,21 @@ public class K8sDeploymentRunnableListener {
                                 .task(runnable.getTask())
                                 .build()
                         )
-                        .build()
-                );
+                        .build();
+
+                    if (log.isTraceEnabled()) {
+                        log.trace("runnable {} {} event {}", clazz.getSimpleName(), runnable.getId(), event);
+                    }
+
+                    // Publish event to Run Manager
+                    eventPublisher.publishEvent(event);
+                }
             }
         }
+    }
+
+    @Autowired
+    public void setEventPublisher(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
     }
 }
