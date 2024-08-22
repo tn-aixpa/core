3fb3dbe431743e202d2659ed177f8baec7302890
Matteo Saloni <matteo.saloni@gmail.com>
2024-08-22 14:49:40 +0200

    fix: add scanning for subpackages to include authorization

--- application/src/main/java/it/smartcommunitylabdhub/core/CoreApplication.java
+++ application/src/main/java/it/smartcommunitylabdhub/core/CoreApplication.java
@@ -2,11 +2,17 @@ package it.smartcommunitylabdhub.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = { "it.smartcommunitylabdhub" })
+@EnableJpaRepositories(basePackages = { "it.smartcommunitylabdhub" })
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@EnableScheduling
+@EntityScan(basePackages = { "it.smartcommunitylabdhub" })
 @ComponentScan(basePackages = { "it.smartcommunitylabdhub" })
 public class CoreApplication {
 
