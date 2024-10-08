6c783b2d0bda9160fab07df238e2f69aeebff57f
Matteo Saloni <matteo.saloni@gmail.com>
2024-10-08 18:43:58 +0200

    fix: fix crontab regex

--- modules/commons/src/main/java/it/smartcommunitylabdhub/commons/Keys.java
+++ modules/commons/src/main/java/it/smartcommunitylabdhub/commons/Keys.java
@@ -11,7 +11,7 @@ public class Keys {
     public static final String FOLDER_PATTERN = ".*\\/$";
     public static final String ZIP_PATTERN = ".*\\.zip$";
     public static final String CRONTAB_PATTERN =
-        "(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+(\\/|-)\\d+)|\\d+|\\*) ?){5,7})";
+        "((((\\d+,)+\\d+|(\\d+(\\/|-|#)\\d+)|\\d+L?|\\*(\\/\\d+)?|L(-\\d+)?|\\?|[A-Z]{3}(-[A-Z]{3})?) ?){5,7})|(@(annually|yearly|monthly|weekly|daily|hourly))";
 
     private Keys() {}
 }
