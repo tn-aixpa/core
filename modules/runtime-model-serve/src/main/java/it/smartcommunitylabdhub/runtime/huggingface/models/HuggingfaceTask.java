7f75530364e0f835a253e30752ef28f9d4aebc7b
kazhamiakin <kazhamiakin@gmail.com>
2024-11-18 08:58:13 +0100

    fix(huggingfaceserve): Correct translation of LLM task types

--- modules/runtime-model-serve/src/main/java/it/smartcommunitylabdhub/runtime/huggingface/models/HuggingfaceTask.java
+++ modules/runtime-model-serve/src/main/java/it/smartcommunitylabdhub/runtime/huggingface/models/HuggingfaceTask.java
@@ -1,11 +1,12 @@
 package it.smartcommunitylabdhub.runtime.huggingface.models;
 
 public enum HuggingfaceTask {
-    sequence_classification("sequence-classification"),
-    token_classification("token-classification"),
-    fill_mask("fill-mask"),
-    text_generation("text-generation"),
-    text2text_generation("text2text-generation");
+    sequence_classification("sequence_classification"),
+    token_classification("token_classification"),
+    fill_mask("fill_mask"),
+    text_generation("text_generation"),
+    text2text_generation("text2text_generation"),
+    text_embedding("text_embedding");
 
     private final String task;
 
