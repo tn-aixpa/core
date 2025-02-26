1f9c5863d9e9729a8f29febf84dc20a0ce08281c
micnori <micnori@users.noreply.github.com>
2025-02-26 09:42:53 +0100

    fix annotations

--- application/src/main/java/it/smartcommunitylabdhub/core/models/indexers/IndexField.java
+++ application/src/main/java/it/smartcommunitylabdhub/core/models/indexers/IndexField.java
@@ -1,7 +1,6 @@
 package it.smartcommunitylabdhub.core.models.indexers;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +8,6 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
--- application/src/main/java/it/smartcommunitylabdhub/core/models/indexers/ItemResult.java
+++ application/src/main/java/it/smartcommunitylabdhub/core/models/indexers/ItemResult.java
@@ -4,6 +4,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ItemResult {
 
     String id;
@@ -16,75 +27,4 @@ public class ItemResult {
     Map<String, Object> metadata = new HashMap<>();
     Map<String, List<String>> highlights = new HashMap<>();
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Map<String, Object> getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Map<String, Object> metadata) {
-        this.metadata = metadata;
-    }
-
-    public Map<String, List<String>> getHighlights() {
-        return highlights;
-    }
-
-    public void setHighlights(Map<String, List<String>> highlights) {
-        this.highlights = highlights;
-    }
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
 }
