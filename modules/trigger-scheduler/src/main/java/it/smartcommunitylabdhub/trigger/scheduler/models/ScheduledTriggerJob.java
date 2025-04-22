3d25390d8363949a3c26563d6d27c5a23b251492
Matteo Saloni <matteo.saloni@gmail.com>
2025-04-22 18:51:12 +0200

    feat: scheduler trigger runs + support quartz expression and simple triggers

--- modules/trigger-scheduler/src/main/java/it/smartcommunitylabdhub/trigger/scheduler/models/ScheduledTriggerJob.java
+++ modules/trigger-scheduler/src/main/java/it/smartcommunitylabdhub/trigger/scheduler/models/ScheduledTriggerJob.java
@@ -1,3 +1,33 @@
 package it.smartcommunitylabdhub.trigger.scheduler.models;
 
-public class ScheduledTriggerJob {}
+import it.smartcommunitylabdhub.commons.models.trigger.TriggerJob;
+import it.smartcommunitylabdhub.trigger.scheduler.SchedulerActuator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScheduledTriggerJob implements TriggerJob {
+
+    private String id;
+    private String user;
+    private String task;
+    private String project;
+
+    private String state;
+    private String message;
+    private String error;
+
+    private String schedule;
+
+    @Override
+    public String getActuator() {
+        return SchedulerActuator.ACTUATOR;
+    }
+}
--- modules/trigger-scheduler/src/main/java/it/smartcommunitylabdhub/trigger/scheduler/quartz/SchedulerJob.java
+++ modules/trigger-scheduler/src/main/java/it/smartcommunitylabdhub/trigger/scheduler/quartz/SchedulerJob.java
@@ -1,11 +1,20 @@
 package it.smartcommunitylabdhub.trigger.scheduler.quartz;
 
-import it.smartcommunitylabdhub.trigger.scheduler.events.TriggerExecutionEvent;
+import it.smartcommunitylabdhub.commons.infrastructure.TriggerRun;
+import it.smartcommunitylabdhub.commons.models.trigger.TriggerEvent;
+import it.smartcommunitylabdhub.commons.models.trigger.TriggerExecutionEvent;
+import it.smartcommunitylabdhub.commons.models.trigger.TriggerJob;
+import it.smartcommunitylabdhub.trigger.scheduler.models.ScheduledTriggerJob;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 @Slf4j
@@ -19,10 +28,34 @@ public class SchedulerJob extends QuartzJobBean {
     }
 
     @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        String triggerId = (String) context.getJobDetail().getJobDataMap().get("triggerId");
-        log.debug("triggered {}", triggerId);
-        TriggerExecutionEvent event = TriggerExecutionEvent.builder().id(triggerId).event("FIRE").build();
+    protected void executeInternal(@NonNull JobExecutionContext context) throws JobExecutionException {
+        if (applicationEventPublisher == null) {
+            log.error("applicationEventPublisher is null");
+            return;
+        }
+
+        //fetch job data and publish FIRE event
+        ScheduledTriggerJob job = (ScheduledTriggerJob) context.getJobDetail().getJobDataMap().get("job");
+
+        log.debug("triggered {}", job.getId());
+        if (log.isTraceEnabled()) {
+            log.trace("job: {}", job);
+        }
+
+        //build a run for this job
+        Map<String, Serializable> details = Map.of(
+            "timestamp",
+            Date.from(Instant.now()).getTime(),
+            "schedule",
+            job.getSchedule()
+        );
+        TriggerRun<TriggerJob> run = TriggerRun.<TriggerJob>builder().job(job).details(details).build();
+
+        TriggerExecutionEvent<TriggerJob> event = TriggerExecutionEvent
+            .builder()
+            .run(run)
+            .event(TriggerEvent.FIRE)
+            .build();
         applicationEventPublisher.publishEvent(event);
     }
 }
