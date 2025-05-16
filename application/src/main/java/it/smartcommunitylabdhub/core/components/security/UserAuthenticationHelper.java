ce4e644a4a404e3ede59a749294beef2085978a2
Matteo Saloni <matteo.saloni@gmail.com>
2025-05-16 12:53:57 +0200

    fix: inflate rebuilt user authentications for trigger and run callbacks

--- application/src/main/java/it/smartcommunitylabdhub/core/components/security/UserAuthenticationHelper.java
+++ application/src/main/java/it/smartcommunitylabdhub/core/components/security/UserAuthenticationHelper.java
@@ -1,6 +1,7 @@
 package it.smartcommunitylabdhub.core.components.security;
 
 import it.smartcommunitylabdhub.authorization.model.UserAuthentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -17,20 +18,21 @@ public class UserAuthenticationHelper {
             return (UserAuthentication<?>) auth;
         }
 
-        // //workaround: inflate basic auth tokens
-        // //TODO define authManager to produce proper authentication
-        // if (auth instanceof UsernamePasswordAuthenticationToken) {
-        //     UserAuthentication<UsernamePasswordAuthenticationToken> user = new UserAuthentication<>(
-        //         (UsernamePasswordAuthenticationToken) auth,
-        //         auth.getName(),
-        //         auth.getAuthorities()
-        //     );
+        //workaround: inflate basic auth tokens
+        //TODO use authManager to produce proper authentication
+        //NOTE: this flow does not propagate ROLE_ADMIN authority
+        if (auth instanceof UsernamePasswordAuthenticationToken) {
+            UserAuthentication<UsernamePasswordAuthenticationToken> user = new UserAuthentication<>(
+                (UsernamePasswordAuthenticationToken) auth,
+                auth.getName(),
+                auth.getAuthorities()
+            );
 
-        //     //update context
-        //     SecurityContextHolder.getContext().setAuthentication(user);
+            //update context
+            SecurityContextHolder.getContext().setAuthentication(user);
 
-        //     return user;
-        // }
+            return user;
+        }
 
         return null;
     }
