730561b2c47c3c1370d475b7994d8b5ec5f8698b
Matteo Saloni <matteo.saloni@gmail.com>
2025-06-11 11:46:07 +0200

    chore: spdx header

--- modules/commons/src/test/java/it/smartcommunitylabhub/commons/utils/TestMapUtils.java
+++ modules/commons/src/test/java/it/smartcommunitylabhub/commons/utils/TestMapUtils.java
@@ -1,3 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * SPDX-License-Identifier: Apache-2.0                  
+ * 
+ */
 package it.smartcommunitylabhub.commons.utils;
 
 import it.smartcommunitylabdhub.commons.utils.MapUtils;
