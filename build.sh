a3a2e73e590c348afe380467db205b6abaa2747a
Matteo Saloni <matteo.saloni@gmail.com>
2025-06-12 12:52:58 +0200

    chore: update build and add a nocache target

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MIN_VERSION=21
-CUR_VERSION=`java -version 2>&1 | grep 'version' 2>&1 | awk -F\" '{ split($2,a,"."); print a[1]}'`
+CUR_VERSION=$(java -version 2>&1 | grep 'version' 2>&1 | awk -F\" '{ split($2,a,"."); print a[1]}')
 
 if [[ "$CUR_VERSION" ]]; then
     echo "Detected java version $CUR_VERSION."
@@ -13,7 +13,19 @@ else
     exit
 fi
 
-
 # Build Root Project
 echo "Building project"
-./mvnw clean install -DskipTests
+./mvnw clean install -pl '!application'
+if [[ $? -ne 0 ]]; then
+    echo "Build failed. Exit."
+    exit 1
+fi
+# Build Application
+echo "Building application"
+./mvnw clean package -pl 'application'
+if [[ $? -ne 0 ]]; then
+    echo "Build failed. Exit."
+    exit 1
+fi
+
+echo "Build completed successfully."
