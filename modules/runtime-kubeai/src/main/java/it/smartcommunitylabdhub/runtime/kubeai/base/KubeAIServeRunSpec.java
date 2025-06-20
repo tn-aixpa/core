e96fdac2e5ce3be4c4a80e12d6c7267373557769
Matteo Saloni <matteo.saloni@gmail.com>
2025-06-20 11:44:53 +0200

    fix: add missing processors param to deserde for kubeai spec

--- modules/runtime-kubeai/src/main/java/it/smartcommunitylabdhub/runtime/kubeai/base/KubeAIServeRunSpec.java
+++ modules/runtime-kubeai/src/main/java/it/smartcommunitylabdhub/runtime/kubeai/base/KubeAIServeRunSpec.java
@@ -6,19 +6,19 @@
 
 /*
  * Copyright 2025 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * https://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 package it.smartcommunitylabdhub.runtime.kubeai.base;
@@ -79,12 +79,13 @@ public class KubeAIServeRunSpec extends RunBaseSpec implements K8sResourceProfil
         KubeAIServeRunSpec spec = mapper.convertValue(data, KubeAIServeRunSpec.class);
         this.taskServeSpec = spec.getTaskServeSpec();
         this.args = spec.getArgs();
-        this.profile = spec.getProfile();
-        this.cacheProfile = spec.getCacheProfile();
         this.env = spec.getEnv();
-        this.scaling = spec.getScaling();
         this.files = spec.getFiles();
         this.secrets = spec.getSecrets();
+        this.profile = spec.getProfile();
+        this.cacheProfile = spec.getCacheProfile();
+        this.processors = spec.getProcessors();
+        this.scaling = spec.getScaling();
     }
 
     public void setTaskServeSpec(KubeAITextServeTaskSpec taskServeSpec) {
